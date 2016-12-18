5bf8f95b4099d8ed462d62d21d3a1c52c6e14fbd
Elek, Márton <elek@users.noreply.github.com>
2016-12-18 15:18:27 +0100

    dependencies for hbase tracing

---
+++ simple/dependencies.sh
@@ -0,0 +1,2 @@
+http://repo1.maven.org/maven2/org/apache/thrift/libthrift/0.9.0/libthrift-0.9.0.jar
+http://repo1.maven.org/maven2/org/apache/htrace/htrace-zipkin/3.1.0-incubating/htrace-zipkin-3.1.0-incubating.jar
