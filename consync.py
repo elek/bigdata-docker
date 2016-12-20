8caf486b90c8c776de0fb7f3f77f050d38f67317
Elek, Márton <elek@users.noreply.github.com>
2016-12-20 16:06:09 +0100

    restructured configuration file + consync could use inotify based listener

--- consync.py
+++ consync.py
@@ -8,28 +8,34 @@ from os import listdir
 from os.path import isfile, join
 from builtins import print
 from jinja2 import Template
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+import logging
+from jinja2 import Environment, FileSystemLoader
 
 url = "http://localhost:8500/v1/kv/"
 
-
+def base_consul_url(prefix):
+    return url + prefix + "/"
 
 def listfiles(dir, prefix):
     for root, dirs, files in os.walk(dir, topdown=True):
         dirs[:] = [d for d in dirs if d != ".git"]
         for file in files:
             filepath = os.path.join(root, file)
-            print("Uploading %s" % filepath)
             upload(filepath, dir, prefix)
 
 
 def upload(path, basedir, prefix):
+    print("Uploading %s" % path)
     content = read_content(path, basedir)
     keypath = os.path.relpath(path, basedir)
-    if not requests.put(url + prefix + "/" + keypath, content).ok:
-        print("Error on uploading file %s to %s" % path, keypath)
+    if not requests.put(base_consul_url(prefix) + keypath, content).ok:
+        print("Error on uploading file {} to {}".format(path, keypath))
 
 def template_render(filepath, basepath, content):
-   t = Template(content)
+   t = Environment(loader=FileSystemLoader(os.path.dirname(filepath))).from_string(content)
    return t.render(env = os.environ)
 
 def find_transformation(filepath, basepath):
@@ -50,12 +56,12 @@ def find_transformation(filepath, basepath):
 
 
 def to_xml(content):
-    result = "<properties>\n"
+    result = "<configuration>\n"
     for line in content.split("\n"):
         keyvalue = line.split(":", 1)
         if len(keyvalue) > 1:
             result += "<property><name>{0}</name><value>{1}</value></property>\n".format(keyvalue[0], keyvalue[1].strip())
-    result += "</properties>"
+    result += "</configuration>"
     return result
 
 
@@ -71,7 +77,7 @@ def read_content(filepath, basepath):
 
 
 def clean(path, prefix):
-    result = requests.get(url + prefix + "?recurse")
+    result = requests.get(base_consul_url(prefix) + "?recurse")
     if result.ok:
         entries = json.loads(result.text)
         for entry in entries:
@@ -79,17 +85,50 @@ def clean(path, prefix):
             filepath = os.path.join(path, relativeKey)
             if not os.path.isfile(filepath):
                 print("Deleting key " + relativeKey)
-                delete_url = url + prefix + "/" + relativeKey
+                delete_url = base_consul_url(prefix) + relativeKey
                 if not requests.delete(delete_url).ok:
                     print("Key delete was unsuccessfull: %s" % delete_url)
     else:
         print("Error on getting existing keys ")
 
 
+class MyEventHandler(FileSystemEventHandler):
+     def __init__(self, basedir, prefix):
+         self.basedir = basedir
+         self.prefix = prefix
+
+     def on_modified(self, event):
+         if not event.is_directory:
+            print(event.event_type)
+            print(event.src_path)
+            upload(event.src_path, self.basedir, self.prefix)
+
 locals()
 parser = argparse.ArgumentParser()
 parser.add_argument("dir", help="directory to upload")
 parser.add_argument("prefix", help="prefix path in consul kv")
+parser.add_argument("--serve", help="Listening for new changes and upload only the changed files", action="store_true")
+parser.add_argument("--url", help="consul server address (protocol, servername, port)")
 args = parser.parse_args()
-clean(args.dir, args.prefix)
-listfiles(args.dir, args.prefix)
+
+if args.url:
+    print(args.url)
+    url = args.url + "/v1/kv/"
+if not args.serve:
+    clean(args.dir, args.prefix)
+    listfiles(args.dir, args.prefix)
+else:
+  logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+  event_handler = MyEventHandler(args.dir, args.prefix)
+  observer = Observer()
+  observer.schedule(event_handler, args.dir, recursive=True)
+  observer.start()
+  try:
+     while True:
+        time.sleep(1)
+  except KeyboardInterrupt:
+     observer.stop()
+  observer.join()
