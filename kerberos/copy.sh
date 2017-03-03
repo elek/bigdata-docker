fe00be0dcbe231925d1ddd72a0151ff13f294682
Elek, Márton <elek@users.noreply.github.com>
2017-03-03 23:45:13 +0100

    switch to automatic keytab and keystore generation for kerberized clusters

--- kerberos/copy.sh
+++
@@ -1,5 +0,0 @@
-docker cp keytab.generate krb5:/data/
-docker exec -it krb5 bash -c 'rm /data/*.keytab'
-docker exec -it krb5 bash -c 'cd /data && ./keytab.generate'
-./keys.sh
-docker cp sc.dn.keystore krb5:/data/ 
